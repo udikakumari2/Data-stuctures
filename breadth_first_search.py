a325f1d531fed8e8a61b50bf8cb1f661ce65631a
udikakumari2 <udikakumari5@gmail.com>
2023-07-06 15:05:53 -0700

    BFS

---
+++ breadth_first_search.py
@@ -0,0 +1,38 @@
+from collections import deque
+
+graph = {
+  'A' : ['S','B'],
+  'B' :[],
+  'S' : ['C', 'G'],
+  'C' : ['D','E','F'],
+  'D' : [],
+  'F' :[],
+  'G' : ['F','H'],
+  'H' : ['E'],
+  'E' : ['H']
+ 
+}
+
+def bfs(graph, node):
+    visited = []
+    # the video has queue as an array. I changed this to deque because popping the first element is O(1) instead of O(n).
+    # see this link for more: https://wiki.python.org/moin/TimeComplexity.
+    queue = deque()
+
+    visited.append(node)
+    queue.append(node)
+
+    while queue:
+        # popleft is O(1). for an array, pop(0) is O(n). hence the change to deque from array.
+        s = queue.popleft()
+        print(s, end = " ")
+
+        for n in graph[s]:
+            if n not in visited:
+                visited.append(n)
+                queue.append(n)
+
+def main():
+    bfs(graph, 'A')
+
+main()
